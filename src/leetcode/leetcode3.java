0b0044d01d82abb3b3e835e8207a9b1e30e0cd01
DP <308775154@qq.com>
2019-10-12 17:48:53 +0800

    no

--- src/leetcode/leetcode3.java
+++ src/leetcode/leetcode3.java
@@ -4,20 +4,26 @@ import java.lang.reflect.Array;
 
 public class leetcode3 {
     public static void main(String[] args) {
-        String str="sdvasdfadadadcaxzvasd";
+        String str="";
        System.out.println(lengthOfLongestSubstring(str));
     }
         static int lengthOfLongestSubstring(String s) {
+
+            if(s.length()<=0)
+                return 0;
             String[] arr=s.split("",s.length());
-            int len=0,maxLen=0,start=0;
+            int len=1,maxLen=1,start=0;
 
             for(int i=1;i<arr.length;i++){
                 if(join(start,i,arr))
                     len++;
-                else
+                else{
                     start=i;
+                    len=1;
+                }
                 if(len>maxLen)
                     maxLen=len;
+                System.out.println("i:"+i+" len:"+len+" start:"+start+" maxlen:"+maxLen);
             }
             return maxLen;
         }
